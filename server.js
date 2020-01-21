03c47cce0451964b26657ef63833f6fcff958068
Greg Roberts <groberts3104@gmail.com>
2020-01-20 19:12:03 -0500

    added app.listen to confirm port working

--- server.js
+++ server.js
@@ -9,3 +9,8 @@ var PORT = process.env.PORT || 8080;
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+app.listen(PORT, function () {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+});
