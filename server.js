50baf851908245761b388ce4efd88a863b2e5f49
Greg Roberts <groberts3104@gmail.com>
2020-01-22 19:10:24 -0500

    added function to parse as json

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ var app = express();
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
-// // Parse request body as JSON
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // // Set Handlebars.
 // var exphbs = require("express-handlebars");
