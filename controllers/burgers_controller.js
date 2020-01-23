950189f57fdc7decec89379d140a0397eb3ef1cc
Greg Roberts <groberts3104@gmail.com>
2020-01-22 19:20:40 -0500

    fixed functions to match functions in orm.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -17,7 +17,7 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burgers", function(req, res) {
-  burger.create(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
+  burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
@@ -28,7 +28,7 @@ router.put("/api/burgers/:id", function(req, res) {
 
   console.log("condition", condition);
 
-  burger.update(
+  burger.updateOne(
     {
       devoured: req.body.devoured
     },
