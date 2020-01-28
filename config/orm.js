60717e53555258292dabdc619109d27b67205cda
Greg Roberts <groberts3104@gmail.com>
2020-01-27 19:50:57 -0500

    added comments for functions

--- config/orm.js
+++ config/orm.js
@@ -1,12 +1,12 @@
+// Import MySQL connection.
 var connection = require("../config/connection.js");
 
+// Helper function for SQL syntax.
 function printQuestionMarks(num) {
     var arr = [];
-
     for (var i = 0; i < num; i++) {
         arr.push("?");
     }
-
     return arr.toString();
 }
 
@@ -20,12 +20,9 @@ function objToSql(ob) {
         var value = ob[key];
         // check to skip hidden properties
         if (Object.hasOwnProperty.call(ob, key)) {
-            // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
             if (typeof value === "string" && value.indexOf(" ") >= 0) {
                 value = "'" + value + "'";
             }
-            // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-            // e.g. {sleepy: true} => ["sleepy=true"]
             arr.push(key + "=" + value);
         }
     }
@@ -34,6 +31,7 @@ function objToSql(ob) {
     return arr.toString();
 }
 
+// Object for all our SQL statement functions.
 var orm = {
     selectAll: function(tableInput, cb) {
         var queryString = "SELECT * FROM " + tableInput + ";";
